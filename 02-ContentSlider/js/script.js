4e7bd59f1c83ab40eb0ae6392c2d84843020c9d9
AntonSmid <anton.smid@triera.net>
2017-07-22 03:10:29 +0200

    content slider - code is to be fixed

---
+++ 02-ContentSlider/js/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    
+// set options
+    var speed = 500;                // fade speed
+    var autoswitch = true;          // auto slider options
+    var autoswitch_speed = 4000;    // auto slider speed
+    
+    // add initial active class
+    $(".slide").first().addClass("active");
+    
+    // Hide all slides
+    $(".slide").hide();
+    
+    // show first slide
+    $(".active").show();
+    
+    // next hendler
+    $("#next").on("click", nextSlide);
+    
+    // prev handler
+    $("#prev").on("click", prevSlide);
+    
+    
+    // auto slider handler
+    if(autoswitch == true) {
+        setInterval(nextSlide, autoswitch_speed);
+    }
+    
+    
+   // to next slide 
+    function nextSlide(){
+        $(".active").removeClass("active").addClass("oldActive");
+        
+        if($(".oldActive").is(":last-child")){
+            $(".slide").first().addClass("active");
+        } else {
+            $(".oldActive").next().addClass("active");
+        }
+        
+        $(".oldActive").removeClass("oldActive");
+        $(".slide").fadeOut(speed);
+        $(".active").fadeIn(speed);
+        
+    };
+    
+    
+    // to prev slide
+    function prevSlide(){
+        $(".active").removeClass("active").addClass("oldActive");
+        if($(".oldActive").is(":first-child")){
+            $(".slide").last().addClass("active");
+        } else {
+            $(".oldActive").prev().addClass("active");
+        }
+        $(".oldActive").removeClass("oldActive");
+        $(".slide").fadeOut(speed);
+        $(".active").fadeIn(speed);
+        
+    };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}) // end ready
+
